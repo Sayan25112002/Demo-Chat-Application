104b06920eae698cb879b23f228d0a1fea3d414b
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-01 11:01:36 +0530

    WebSocketEventListener Modified

--- src/main/java/com/example/ChatApplication/config/WebSocketEventListener.java
+++ src/main/java/com/example/ChatApplication/config/WebSocketEventListener.java
@@ -1,10 +1,13 @@
 package com.example.ChatApplication.config;
 
+import com.example.ChatApplication.dto.requestDto.ChatMessageRequestDto;
+import com.example.ChatApplication.entity.ennum.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Component
@@ -12,8 +15,20 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations messagingTemplate;
+
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        String userName = (String) accessor.getSessionAttributes().get("userName");
+        if(userName!=null){
+            log.info("User Disconnected: {}"+userName);
+            var chatMessageRequestDto = ChatMessageRequestDto.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(userName)
+                    .build();
+            messagingTemplate.convertAndSend("/topic/public", chatMessageRequestDto);
 
+        }
     }
 }
