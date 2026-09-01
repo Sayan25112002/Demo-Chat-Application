a063dd436bd358aa0358f2acd9d6ee0fdb7d11a1
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-01 10:52:36 +0530

    ChatController Added and Modified

--- src/main/java/com/example/ChatApplication/controller/ChatController.java
+++ src/main/java/com/example/ChatApplication/controller/ChatController.java
@@ -2,10 +2,12 @@ package com.example.ChatApplication.controller;
 
 import com.example.ChatApplication.dto.requestDto.ChatMessageRequestDto;
 import com.example.ChatApplication.dto.responseDto.ChatMessageResponseDto;
+import com.example.ChatApplication.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ChatController {
 
-    private final SimpMessageSendingOperations simpMessageSendingOperations;
+    private final ChatService chatService;
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessageResponseDto sendMessage(@Payload ChatMessageRequestDto chatMessageRequestDto) {
+        return chatService.sendMessage(chatMessageRequestDto);
+    }
 
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessageResponseDto addUser(@Payload ChatMessageRequestDto chatMessageRequestDto, SimpMessageHeaderAccessor headerAccessor) {
+        return chatService.addUser(chatMessageRequestDto,headerAccessor);
     }
 }
