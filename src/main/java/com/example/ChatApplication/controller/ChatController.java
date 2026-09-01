ceb6dddffdf8f56b8026d815a547945bee530858
Sayan25112002 <sayanmajumdar1920014@gmail.com>
2026-09-01 11:04:51 +0530

    ChatController Modified

--- src/main/java/com/example/ChatApplication/controller/ChatController.java
+++ src/main/java/com/example/ChatApplication/controller/ChatController.java
@@ -8,7 +8,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
